4d40d8047966c64732aa68da19dd4dc8736e7a0f
J4ZZ <Blastress05@gmail.com>
2025-08-09 22:29:29 +0700

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -14,7 +14,10 @@ export default function About(){
         <div style={{marginTop:12, display:'grid', gap:8}}>
           <div style={{display:'flex', gap:12, alignItems:'center'}}>
             <div style={{width:10,height:10,background:'#fff',borderRadius:4}}></div>
-            <div style={{color:'var(--muted)'}}>Misi: Membentuk pemain catur yang berintegritas dan berkompeten.</div>
+            <div style={{color:'var(--muted)'}}>Misi <tr><ul>Mengembangkan kemampuan dan keterampilan catur mahasiswa melalui pelatihan rutin, pembinaan terarah, dan kegiatan kompetitif.
+<ul> <ul>Menciptakan lingkungan yang kondusif untuk belajar, berlatih, dan bertukar pengetahuan antarpemain catur.<ul> <ul>Menumbuhkan sportivitas, disiplin, dan rasa kebersamaan dalam setiap aktivitas klub.
+<ul> <ul>Mengikuti serta menyelenggarakan berbagai turnamen catur sebagai ajang peningkatan prestasi dan pengalaman.
+<ul> <ul>Menjadi duta yang membawa nama baik Universitas Satya Terra Bhinneka melalui prestasi di bidang catur.<ul></tr></div>
           </div>
           <div style={{display:'flex', gap:12, alignItems:'center'}}>
             <div style={{width:10,height:10,background:'#fff',borderRadius:4}}></div>
@@ -25,3 +28,4 @@ export default function About(){
     </div>
   )
 }
+
