f9b3ee5e9b4a861712c741961a89d6bfbc2733a4
J4ZZ <Blastress05@gmail.com>
2025-08-12 00:38:36 +0700

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer(){
-  return (
-    <footer className="footer">
-      © {new Date().getFullYear()} UPM Chess Club — Dibuat Dengan Cinta & React Oleh Jasswant 🥰
-    </footer>
-  )
-}
